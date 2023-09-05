6d4e3f6b191c2fa80b75d95885f6d05c455cc3b2
Altyn777 <darzeeia@gmail.com>
2023-09-05 23:16:10 +0300

    clean

--- src/components/ReactComponent.tsx
+++
@@ -1,7 +0,0 @@
-import reactLogo from "../assets/react.svg";
-
-export const ReactComponent = () => (
-  <a href="https://react.dev" target="_blank">
-    <img src={reactLogo} className="logo" alt="Vite logo" />
-  </a>
-);
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,8 +1,6 @@
 // Import the components
 import { MyComponent } from "./MyComponent";
-import { ViteComponent } from "./ViteComponent";
-import { ReactComponent } from "./ReactComponent";
 
 // Export the components with aliases and as default
-export { MyComponent, ViteComponent, ReactComponent };
-export default { MyComponent, ViteComponent, ReactComponent };
+export { MyComponent };
+export default { MyComponent };
